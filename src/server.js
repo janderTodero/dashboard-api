8bc688d9700c0e10ee7298af1b622791ac0456aa
janderTodero <jandertodero@gmail.com>
2025-07-25 16:07:33 -0300

    Authentication created

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
 
-dotenv.config()
+const authRoutes = require("./routes/authRoutes")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API dashboard')
-})
+app.use("/api/auth", authRoutes)
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB conectado')
