0db8f66c8444383854e1f58fe379ca13ed32fb9d
janderTodero <jandertodero@gmail.com>
2025-07-29 19:39:09 -0300

    transactions controller

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,5 +1,6 @@
 const Transaction = require('../models/Transaction')
 
+
 exports.create = async (req, res) => {
     const { title, amount, type, category, date } = req.body
     const userId = req.user.id
@@ -19,5 +20,33 @@ exports.create = async (req, res) => {
         res.status(201).json(newTransaction)
     } catch (error) {
         res.status(500).json({message: "Error creating transaction", error: error.message})
+    }
+}
+
+exports.getAllTransactions = async (req, res) => {
+    const userId = req.user.id
+
+    try {
+        const transactions = await Transaction.find({user: userId}).sort({date: -1 })
+        res.status(200).json(transactions)
+    } catch (error) {
+        res.status(500).json({message: "Error to get transactions", error: error.message})
+    }
+}
+
+exports.readTransaction = async (req, res) => {
+    const transactionId = req.params.id
+    const userId = req.user.id
+
+    try {
+        const transaction = await Transaction.findOne({_id: transactionId, user: userId})
+
+        if(!transaction) {
+            return res.status(404).json({message: "Transaction not found"})
+        }
+
+        res.status(200).json(transaction)
+    } catch (error) {
+        res.status(500).json({message: "Error to find transaction", error: error.message})
     }
 }
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { create } = require('../controllers/transactionController')
+const { create, getAllTransactions, readTransaction } = require('../controllers/transactionController')
 const authMiddleware = require('../middlewares/authMiddleware')
 const router = express.Router()
 
 router.use(authMiddleware)
 
 router.post("/", create)
+router.get("/", getAllTransactions)
+router.get("/:id", readTransaction)
 
 module.exports = router
