4225053607d9f125827d0b94ee038b2b02607834
janderTodero <jandertodero@gmail.com>
2025-08-19 19:49:44 -0300

    more limit fix

--- src/controllers/limitController.js
+++ src/controllers/limitController.js
@@ -1,49 +1,49 @@
-const Limit = require("../models/Limit")
+const Limit = require("../models/Limit");
 
 exports.createLimit = async (req, res) => {
-    const { limit } = req.body
-    const userId = req.user.id
+    const { limit } = req.body;
+    const userId = req.user.id;
 
     try {
         const newLimit = new Limit({
             limit: limit,
             user: userId,
-        })
+        });
 
-        await newLimit.save()
-        res.status(201).json(newLimit)
+        await newLimit.save();
+        res.status(201).json({ limit: newLimit });
     } catch (error) {
-        res.status(500).json({ message: "Error to create a limit"})
+        res.status(500).json({ message: "Error to create a limit", error: error.message });
     }
-}
+};
 
 exports.getLimit = async (req, res) => {
-    const userId = req.user.id
+    const userId = req.user.id;
 
     try {
-            const limit = await Limit.find({user: userId})
-            res.status(200).json(limit)
-        } catch (error) {
-            res.status(500).json({message: "Error to get transactions", error: error.message})
-        }
-}
+        const limit = await Limit.findOne({ user: userId });
+        res.status(200).json({ limit });
+    } catch (error) {
+        res.status(500).json({ message: "Error to get limit", error: error.message });
+    }
+};
 
 exports.changeLimit = async (req, res) => {
-    const userId = req.user.id
-    const { limit } = req.body
+    const userId = req.user.id;
+    const { limit } = req.body;
 
     try {
-        let updatedLimit = await Limit.findOne({ user: userId })
+        let updatedLimit = await Limit.findOne({ user: userId });
 
-        if(updatedLimit) {
+        if (updatedLimit) {
             updatedLimit.limit = limit;
-            await limit.save()
+            await updatedLimit.save();
         } else {
-            limit = await Limit.create({user: userId, value})
+            updatedLimit = await Limit.create({ user: userId, limit });
         }
 
-        return res.status(200).json({message: "Limite alterado com sucesso!", limit})
+        return res.status(200).json({ message: "Limite alterado com sucesso!", limit: updatedLimit });
     } catch (error) {
-        return res.status(500).json({ message: "Erro ao atualizar o limite", error: error.message})
+        return res.status(500).json({ message: "Erro ao atualizar o limite", error: error.message });
     }
-}
+};
